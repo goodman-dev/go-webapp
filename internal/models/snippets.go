2d51f44723ada0cf75dd0e2ed8598bfe09a38ffc
GoodmanDev <david.g.goodman@bath.edu>
2024-08-23 01:08:19 +0100

    Wrap our insert in a db transaction

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -26,7 +26,21 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 	statement := `INSERT INTO snippets (title, content, created, expires) 
 	VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
-	result, err := m.DB.Exec(statement, title, content, expires)
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	// If Commit() has already been called when tx.Rollback() is
+	// called, tx.Rollback() will be a no-op
+	defer tx.Rollback()
+
+	result, err := tx.Exec(statement, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	err = tx.Commit()
 	if err != nil {
 		return 0, err
 	}
